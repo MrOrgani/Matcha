23b95c442a3ee0b248483dc86f15100784f34921
valentin Lecoq-Vallon <vlecoq-v@student.42.fr>
2019-06-26 14:56:54 +0200

    added the routing on the  main page and reorganised the folder structure

---
+++ client/src/Components/Navbar/NavBar.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+// import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  bar: {
+    background: "transparent",
+    boxShadow: "none"
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1,
+    color: "black"
+  },
+  text: {
+    color: "black"
+  }
+}));
+
+function NavBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.bar}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            aria-label="Menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Matcha
+          </Typography>
+          <Link to="/login">
+            <p className={classes.text}>Login</p>
+          </Link>
+          {/* <Button>Login</Button> */}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default NavBar;
