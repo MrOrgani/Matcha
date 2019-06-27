3e3c4d31f68c9a7d72e319ea4d2131a27e270c63
Maxime ORGANI <morgani@e3r1p10.42.fr>
2019-06-27 13:08:01 +0200

    seedage DB fonctionnel

--- API/app.js
+++ API/app.js
@@ -1,48 +1,66 @@
 var createError = require("http-errors");
-var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var indexRoute = require("./routes/index");
 var testAPIRouter = require("./routes/testAPI");
 var cors = require("cors");
 
+var express = require("express");
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// // view engine setup
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(logger("dev"));
+// app.use(express.json());
+// app.use(cors());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/testAPI", testAPIRouter);
-// app.use(express.cookieParser());
+// ==================== MAXIME'S CODE TO AUTH USER =============================
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+const authRoute = require("./routes/auth");
+
+const neo4j = require("neo4j-driver").v1;
+const driver = neo4j.driver(
+  "bolt://localhost",
+  neo4j.auth.basic("neo4j", "2j54A%")
+);
+const session = driver.session();
+
+app.use("/api/user", authRoute);
+
+app.use("/", function(req, res) {
+  res.send("API is working properly");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// ============================================================================
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+// app.use("/", indexRoute);
+// app.use("/users", usersRoute);
+// app.use("/testAPI", testAPIRouter);
+// // app.use(express.cookieParser());
 
-// const port = 5000;
-// app.listen(port, () => console.log(`Connect on port ${port}`));
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-module.exports = app;
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
+
+const port = 5000;
+app.listen(port, () => console.log(`Connect on port ${port}`));
+
+// module.exports = app;
