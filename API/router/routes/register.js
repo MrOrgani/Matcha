4fb7524b73905d372964d638f7bc0d3869f20c79
valentin Lecoq-Vallon <vlecoq-v@student.42.fr>
2019-06-27 15:18:17 +0200

    working on api structure

--- API/router/routes/register.js
+++ API/router/routes/register.js
@@ -8,9 +8,10 @@ router
     res.send("API is working properly");
   })
   .post("/", function(req, res) {
-    res.writeHead(200, { "Content-Type": "text/event-stream" });
     console.log(req);
-    req.send("We received a post request");
+
+    res.writeHead(200);
+    res.end("We received a post request");
   });
 
 module.exports = router;
