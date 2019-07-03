6e646e2884b3b03657c76daa53b8fbb6e28519a4
Maxime ORGANI <morgani@e1r2p2.42.fr>
2019-07-03 18:30:35 +0200

    Page search & liked button

--- client/src/Scenes/Search/Components/User.js
+++ client/src/Scenes/Search/Components/User.js
@@ -1,10 +1,111 @@
+// import React from "react";
+// import "../public/stylesheet/style.css";
+
+// const User = props => {
+//   return (
+//     <div className="profile">
+//       <img src={props.value.picMedium} />
+//     </div>
+//   );
+// };
+
+// export default User;
+
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 150,
+    margin: 10,
+    height: 150
+  },
+  media: {
+    height: 0,
+    paddingTop: "150%" // 16:9
+  },
+  root: {
+    // height: 0,
+    // paddingTop: "150%", // 16:9,
+    marginTop: 100
+  },
+  button1: {
+    color: "white"
+  },
+  button2: {
+    color: "red"
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandOpen: {
+    transform: "rotate(180deg)"
+  },
+  avatar: {
+    backgroundColor: red[500]
+  }
+}));
 
 const User = props => {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+  const [colored, setColored] = React.useState(false);
+
+  function handleClick() {
+    setExpanded(!expanded);
+  }
+
+  function handleColor() {
+    setColored(!colored);
+  }
+
   return (
-    <div>
-      <h3>{props.name}</h3>
-    </div>
+    <CardMedia className={classes.card} image={props.value.picLarge}>
+      <div className={classes.root}>
+        <IconButton
+          className={colored ? classes.button2 : classes.button1}
+          aria-label="Add to favorites"
+          onClick={handleColor}
+        >
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded
+          })}
+          onClick={handleClick}
+          aria-expanded={expanded}
+          aria-label="Show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </div>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography>{props.value.firstName}</Typography>
+          <Typography paragraph>{props.value.bio}</Typography>
+        </CardContent>
+      </Collapse>
+    </CardMedia>
   );
 };
 
--- client/src/Scenes/Search/Components/UserList.js
+++ client/src/Scenes/Search/Components/UserList.js
@@ -1,16 +1,17 @@
 import React, { useState, useContext } from "react";
 import User from "./User";
 import { UsersContext } from "./UsersContext";
+import "../public/stylesheet/style.css";
 
 const UserList = () => {
   const [users, setUsers] = useContext(UsersContext);
-  console.log(users);
+
+  users.map(user => console.log(user._fields[0].properties));
   return (
-    <div>
-      {/* {users.map(user => (
-        <User name={user.name} />
-      ))} */}
-      ok
+    <div className="container">
+      {users.map((user, index) => (
+        <User key={index} value={user._fields[0].properties} />
+      ))}
     </div>
   );
 };
