f35f2201cc4e155f2a0ce23fc0b9080ce589d0f5
valentin Lecoq-Vallon <vlecoq-v@student.42.fr>
2019-07-04 15:31:42 +0200

    small modif

--- client/src/App.js
+++ client/src/App.js
@@ -20,30 +20,4 @@ function App() {
   );
 }
 
-// class App extends React.Component {
-//   constructor() {
-//     super();
-//   }
-//   //   this.state = {
-//   //     apiResponse: ""
-//   //   };
-//   // }
-
-//   // // callAPI() {
-//   // //   fetch("http://localhost:9000/testAPI")
-//   // //     // .then(res => console.log(res))
-//   // //     .then(res => res.text())
-//   // //     .then(res => this.setState({ apiResponse: res }))
-//   // //     .then(err => err);
-//   // // }
-
-//   // // componentDidMount() {
-//   // //   this.callAPI();
-//   // // }
-
-//   render() {
-// ;
-//   }
-// }
-
 export default App;
