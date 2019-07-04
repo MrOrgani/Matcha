5952bf07d6e846504772ab76ee394f9385d948e1
Maxime ORGANI <morgani@e1r2p20.42.fr>
2019-07-04 17:14:24 +0200

    menu

--- client/src/Components/Navbar/DrawerNavigation.js
+++ client/src/Components/Navbar/DrawerNavigation.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
-import Button from "@material-ui/core/Button";
+// import Button from "@material-ui/core/Button";
 import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
+// import InboxIcon from "@material-ui/icons/MoveToInbox";
+// import MailIcon from "@material-ui/icons/Mail";
 import HomeIcon from "@material-ui/icons/Home";
 import SearchIcon from "@material-ui/icons/Search";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import { Link } from "react-router-dom";
+import CloseIcon from "@material-ui/icons/Close";
 
 import MenuButton from "./Components/MenuButton";
+import RangeSlider from "./Components/RangeSlider";
+import { grey } from "@material-ui/core/colors";
 
 const useStyles = makeStyles({
   list: {
@@ -30,6 +33,13 @@ const useStyles = makeStyles({
     textDecoration: "none",
     opacity: 0.54,
     color: "black"
+  },
+  top: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    textDecoration: "none",
+    color: "white"
+    // opacity: 0.54
+    // color: "black"
   }
 });
 
@@ -49,43 +59,71 @@ export default function TemporaryDrawer() {
 
     setState({ ...state, [side]: open });
   };
+  const path = window.location.pathname;
 
   const sideList = side => (
     <div
       className={classes.list}
       role="presentation"
-      onClick={toggleDrawer(side, false)}
+      // onClick={toggleDrawer(side, false)}
       onKeyDown={toggleDrawer(side, false)}
     >
-      <List>
-        {["Home", "Connect", "Search"].map((text, index) => (
-          <ListItem button key={text}>
+      <div className={classes.top}>
+        <List>
+          <ListItem>
             <ListItemIcon>
-              {text === "Home" && <HomeIcon />}
-              {text === "Search" && <SearchIcon />}
-              {text === "Connect" && <AccountCircleIcon />}
-            </ListItemIcon>
-            <ListItemText>
-              {text === "Home" && (
-                <Link to="/" className={classes.link}>
-                  {text}
-                </Link>
-              )}
-              {text === "Search" && (
-                <Link to="/Search" className={classes.link}>
-                  {text}
-                </Link>
-              )}
-              {text === "Connect" && (
-                <Link to="/signIn" className={classes.link}>
-                  {text}
-                </Link>
-              )}
-            </ListItemText>
+              <CloseIcon onClick={toggleDrawer(side, false)} />
+            </ListItemIcon>{" "}
+            <ListItemText>Matcha</ListItemText>
           </ListItem>
+        </List>
+      </div>
+      <List>
+        {["Home", "Connect", "Search"].map(text => (
+          <div key={text}>
+            {text === "Home" && (
+              <Link to="/" className={classes.link}>
+                <ListItem button key={text}>
+                  <ListItemIcon>
+                    <HomeIcon />
+                  </ListItemIcon>
+                  <ListItemText>{text}</ListItemText>{" "}
+                </ListItem>
+              </Link>
+            )}
+            {text === "Search" && (
+              <Link to="/Search" className={classes.link}>
+                <ListItem button key={text}>
+                  <ListItemIcon>
+                    <SearchIcon />
+                  </ListItemIcon>
+                  <ListItemText>{text}</ListItemText>{" "}
+                </ListItem>
+              </Link>
+            )}
+            {text === "Connect" && (
+              <Link to="/signIn" className={classes.link}>
+                <ListItem button key={text}>
+                  <ListItemIcon>
+                    <AccountCircleIcon />
+                  </ListItemIcon>
+                  <ListItemText>{text}</ListItemText>{" "}
+                </ListItem>
+              </Link>
+            )}
+          </div>
         ))}
       </List>
       <Divider />
+      {path === "/Search" && (
+        <div>
+          <ListItemText>Age</ListItemText>
+          <RangeSlider />
+          <ListItemText>Gender</ListItemText>
+          <ListItemText>Distance</ListItemText>
+          <Divider />
+        </div>
+      )}
       <List>
         {["About us"].map((text, index) => (
           <ListItem button key={text}>
@@ -101,7 +139,10 @@ export default function TemporaryDrawer() {
       <div onClick={toggleDrawer("left", true)} className={classes.icon}>
         <MenuButton />
       </div>
-      <Drawer open={state.left} onClose={toggleDrawer("left", false)}>
+      <Drawer
+        open={state.left}
+        // onClose={toggleDrawer("left", false)}
+      >
         {sideList("left")}
       </Drawer>
     </div>
