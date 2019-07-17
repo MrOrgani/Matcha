b7f91ef300030391775156aebca4bd1da897a889
valentin Lecoq-Vallon <vlecoq-v@student.42.fr>
2019-07-17 16:00:20 +0200

    adapted return from login

--- API/controlers/User.js
+++ API/controlers/User.js
@@ -8,16 +8,17 @@ async function createUser(req, res) {
   try {
     if (await modelUser.findOne(req.body.login, "login"))
       return res.status(206).send(`login is taken`);
+  } catch (err) {
+    res.status(206).send(err);
+  }
+  let errors = await Validation.RegisterValidation(req.body);
+  if (!isEmpty(errors)) return res.status(206).send(errors);
 
-    let errors = await Validation.RegisterValidation(req.body);
-    if (!isEmpty(errors)) return res.status(206).send(errors);
-
-    //create db ready userinfo with uuid for jwt
-    const user = await cryptAndObjectify(req);
-    user.uuid = await uuid();
-
+  //create db ready userinfo with uuid for jwt
+  const user = await cryptAndObjectify(req);
+  user.uuid = await uuid();
+  try {
     const data = await modelUser.createUser(user, res);
-    // console.log("data", data);
     res.status(200).send(data);
   } catch (err) {
     res.status(206).send(err);
@@ -25,44 +26,37 @@ async function createUser(req, res) {
 }
 
 async function loginUser(req, res) {
+  const user = await cryptAndObjectify(req);
+  let errors = await Validation.LoginValidation(req.body);
+  if (!isEmpty(errors)) return res.status(206).send(errors);
+
   try {
-    const user = await cryptAndObjectify(req);
-    let errors = await Validation.LoginValidation(req.body);
-    if (!isEmpty(errors)) return res.status(206).send(errors);
-
-    const PropNodeExists = await modelUser.findOne(req.body.login, "login");
-    if (!PropNodeExists) return res.status(206).send("Invalid username");
-    const { login, password, uuid, email } = await modelUser.connect(
-      user.login,
-      "login"
-    );
-    const userData = { login, password, uuid, email };
-    // console.log("user data in users controller", userData);
-    if (isEmpty(userData)) return res.status(206).send("Invalid username");
-    const isValidPwd = await bcrypt.compare(
-      req.body.password,
-      userData.password
-    );
-    if (isValidPwd == false) return res.status(206).send("Invalid password");
-    delete userData.password;
-
-    // JWT auth token
-    // console.log("return from connection", userData);
-    const token = jwt.sign({ uuid: userData.uuid }, process.env.TOKEN_SECRET);
-    userData.uuid = token;
-    res
-      .header("auth-token", token)
-      .status(200)
-      .send(userData);
+    if (!(await modelUser.findOne(req.body.login, "login")))
+      return res.status(206).send("Invalid username");
   } catch (err) {
     res.status(206).send(err);
   }
+
+  const userData = await modelUser.findOne(user.login, "login");
+  // const userData = { login, password, uuid, email };
+  console.log("user data in users controller", userData);
+  if (isEmpty(userData)) return res.status(206).send("Invalid username");
+  if (!(await bcrypt.compare(req.body.password, userData.password)))
+    return res.status(206).send("Invalid password");
+  delete userData.password;
+
+  // JWT auth token
+  const token = jwt.sign({ uuid: userData.uuid }, process.env.TOKEN_SECRET);
+  userData.uuid = token;
+  res
+    // .header("auth-token", token)
+    .status(200)
+    .send(userData);
 }
 
 // Crypts pwd and returns a well rounded user object from req.body
 async function cryptAndObjectify(req) {
   const salt = await bcrypt.genSalt(10);
-  // console.log("salt: ", salt);
   const hashPassword = await bcrypt.hash(req.body.password, salt);
 
   const user = {
--- API/models/modelUser.js
+++ API/models/modelUser.js
@@ -20,19 +20,6 @@ async function createUser(req, res) {
   return data;
 }
 
-async function connect(value) {
-  const data = await session.run(
-    `MATCH(u:User)
-    WHERE u.login = $value
-    RETURN u`,
-    {
-      value
-    }
-  );
-  // console.log(data);
-  if (data.records[0]) return data.records[0]._fields[0].properties;
-}
-
 async function findOne(req, category) {
   const data = await session.run(
     `WITH {category} AS propname
@@ -101,7 +88,6 @@ async function getUsers(req, res) {
 
 module.exports = {
   createUser,
-  connect,
   gUsers,
   delUsers,
   getUsers,
