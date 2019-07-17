864af71a1c8d7969f0e0ed13308f39a963e7c4db
Valentin LECOQ-VALLON <vlecoq-v@e1r12p14.42.fr>
2019-07-17 17:56:47 +0200

    refacturing userContext and var names

--- client/src/Scenes/Search/Search.js
+++ client/src/Scenes/Search/Search.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import UserList from "./Components/UserList";
 import { UsersProvider } from "./Components/UsersContext";
+import axios from "axios";
 // import { FiltersProvider } from "./Components/FiltersContext";
 import "./public/stylesheet/style.css";
 // import Container from "@material-ui/core/Container";
@@ -13,13 +14,15 @@ const Search = () => {
   const [gender, setGender] = useState("both");
   const [age, setAge] = useState([18, 100]);
 
-  const handleChange = (event, newValue) => {
+  const handleChange = (event, newAge) => {
     if (event.target.name === "gender") setGender(event.target.value);
     else {
-      setAge(newValue);
+      setAge(newAge);
     }
     // console.log(event);
   };
+
+  console.log(axios("http://localhost:9000/"));
 
   return (
     <div>
