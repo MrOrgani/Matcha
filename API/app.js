7fdf7749b86bb62f52612e053abec42b2a73a418
Valentin LECOQ-VALLON <vlecoq-v@e1r9p13.42.fr>
2019-07-23 11:56:07 +0200

    finished with front to back blocking system

--- API/app.js
+++ API/app.js
@@ -1,29 +1,22 @@
-const createError = require("http-errors");
+// const createError = require("http-errors");
 const express = require("express");
+// const server = require("http").Server(app);
 require("dotenv").config(); //STORE PASSWORD AND LOGIN IN .ENV
-const path = require("path");
+// const path = require("path");
 const cookieParser = require("cookie-parser");
 // const logger = require("morgan");
 const cors = require("cors");
 const app = express();
-const bodyParser = require("body-parser");
+// const socket = require("socket.io").listen(httpServer);
 
 //MIDDLEWARE
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(logger("dev"));
-// app.use(express.static(path.join(__dirname, "public")));
 
 // DIRIGE VERS LE ROUTER CENTRAL
 const router = require("./router");
 app.use("/", router);
-
-// app.use("/", indexRoute);
-// app.use("/users", usersRoute);
-// app.use("/testAPI", testAPIRouter);
-// // app.use(express.cookieParser());
 
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
@@ -40,8 +33,5 @@ app.use("/", router);
 //   res.status(err.status || 500);
 //   res.render("error");
 // });
-
-// const port = 5000;
-// app.listen(port, () => console.log(`Connect on port ${port}`));
 
 module.exports = app;
