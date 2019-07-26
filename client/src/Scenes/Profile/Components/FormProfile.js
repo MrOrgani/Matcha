a22664393f88976a344e84ba45923a710ae561e1
Maxime ORGANI <morgani@e1r7p12.42.fr>
2019-07-26 13:07:23 +0200

    TempChat

--- client/src/Scenes/Profile/Components/FormProfile.js
+++ client/src/Scenes/Profile/Components/FormProfile.js
@@ -1,14 +1,15 @@
-import React, { useContext } from "react"; // , { useState }
-import { Formik } from "formik";
-import axios from "axios";
-import { makeStyles } from "@material-ui/core/styles";
-import Select from "@material-ui/core/Select";
-import OutlinedInput from "@material-ui/core/OutlinedInput";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+import React from // , { useContext }
+"react"; // , { useState }
+// import { Formik } from "formik";
+// import axios from "axios";
+// import { makeStyles } from "@material-ui/core/styles";
+// import Select from "@material-ui/core/Select";
+// import OutlinedInput from "@material-ui/core/OutlinedInput";
+// import Button from "@material-ui/core/Button";
+// import Typography from "@material-ui/core/Typography";
 
 import useProfileForm from "./useProfileForm";
-import { ProfileFormContext } from "./ProfileFormContext";
+// import { ProfileFormContext } from "./ProfileFormContext";
 
 import {
   FirstName,
@@ -22,46 +23,46 @@ import {
   Submit
 } from "./Components/FieldsForm";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    height: "auto"
-  },
-  image: {
-    backgroundImage: "url(https://source.unsplash.com/random)",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    backgroundPosition: "center"
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center"
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1)
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2)
-  },
-  gallery: {
-    display: "inline",
-    width: "50%"
-  },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1)
-  }
-}));
+// const useStyles = makeStyles(theme => ({
+//   root: {
+//     height: "auto"
+//   },
+//   image: {
+//     backgroundImage: "url(https://source.unsplash.com/random)",
+//     backgroundRepeat: "no-repeat",
+//     backgroundSize: "cover",
+//     backgroundPosition: "center"
+//   },
+//   paper: {
+//     margin: theme.spacing(8, 4),
+//     display: "flex",
+//     flexDirection: "column",
+//     alignItems: "center"
+//   },
+//   avatar: {
+//     margin: theme.spacing(1),
+//     background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"
+//   },
+//   form: {
+//     width: "100%", // Fix IE 11 issue.
+//     marginTop: theme.spacing(1)
+//   },
+//   submit: {
+//     margin: theme.spacing(3, 0, 2)
+//   },
+//   gallery: {
+//     display: "inline",
+//     width: "50%"
+//   },
+//   textField: {
+//     marginLeft: theme.spacing(1),
+//     marginRight: theme.spacing(1)
+//   }
+// }));
 
 function FormProfile() {
-  const { handleSubmit, values } = useProfileForm();
-  const classes = useStyles();
+  const { handleSubmit } = useProfileForm();
+  // const classes = useStyles();
   return (
     <form onSubmit={handleSubmit}>
       <FirstName />
--- client/src/Scenes/Profile/Components/Photos.js
+++ client/src/Scenes/Profile/Components/Photos.js
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React from // , { useContext }
+"react";
 // import { makeStyles } from "@material-ui/core/styles";
 // import PhotoMenuBar from "./Components/PhotoMenuBar";
 // import axios from "axios";
@@ -49,67 +50,107 @@ import React, { useContext } from "react";
 // }
 
 import { Upload, Icon, Modal } from "antd";
+import useProfileForm from "./useProfileForm";
 
-function getBase64(file) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = error => reject(error);
-  });
-}
+// function getBase64(file) {
+//   return new Promise((resolve, reject) => {
+//     const reader = new FileReader();
+//     reader.readAsDataURL(file);
+//     reader.onload = () => resolve(reader.result);
+//     reader.onerror = error => reject(error);
+//   });
+// }
 
-class PicturesWall extends React.Component {
-  state = {
-    previewVisible: false,
-    previewImage: "",
-    fileList: []
-  };
+// class PicturesWall extends React.Component {
+//   state = {
+//     previewVisible: false,
+//     previewImage: "",
+//     fileList: []
+//   };
 
-  handleCancel = () => this.setState({ previewVisible: false });
+//   handleCancel = () => this.setState({ previewVisible: false });
 
-  handlePreview = async file => {
-    if (!file.url && !file.preview) {
-      file.preview = await getBase64(file.originFileObj);
-    }
+//   handlePreview = async file => {
+//     if (!file.url && !file.preview) {
+//       file.preview = await getBase64(file.originFileObj);
+//     }
 
-    this.setState({
-      previewImage: file.url || file.preview,
-      previewVisible: true
-    });
-  };
+//     this.setState({
+//       previewImage: file.url || file.preview,
+//       previewVisible: true
+//     });
+//   };
 
-  handleChange = ({ fileList }) => this.setState({ fileList });
+//   handleChange = ({ fileList }) => this.setState({ fileList });
 
-  render() {
-    const { previewVisible, previewImage, fileList } = this.state;
-    const uploadButton = (
-      <div>
-        <Icon type="plus" />
-        <div className="ant-upload-text">Upload</div>
-      </div>
-    );
-    return (
-      <div className="clearfix">
-        <Upload
-          action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-          listType="picture-card"
-          fileList={fileList}
-          onPreview={this.handlePreview}
-          onChange={this.handleChange}
-        >
-          {fileList.length >= 8 ? null : uploadButton}
-        </Upload>
-        <Modal
-          visible={previewVisible}
-          footer={null}
-          onCancel={this.handleCancel}
-        >
-          <img alt="example" style={{ width: "100%" }} src={previewImage} />
-        </Modal>
-      </div>
-    );
-  }
-}
+//   render() {
+//     const { previewVisible, previewImage, fileList } = this.state;
+//     const uploadButton = (
+//       <div>
+//         <Icon type="plus" />
+//         <div className="ant-upload-text">Upload</div>
+//       </div>
+//     );
+//     return (
+//       <div className="clearfix">
+//         <Upload
+//           action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+//           listType="picture-card"
+//           fileList={fileList}
+//           onPreview={this.handlePreview}
+//           onChange={this.handleChange}
+//         >
+//           {fileList.length >= 8 ? null : uploadButton}
+//         </Upload>
+//         <Modal
+//           visible={previewVisible}
+//           footer={null}
+//           onCancel={this.handleCancel}
+//         >
+//           <img alt="example" style={{ width: "100%" }} src={previewImage} />
+//         </Modal>
+//       </div>
+//     );
+//   }
+// }
+
+const PicturesWall = () => {
+  const {
+    values,
+    handlePreview,
+    handleChangePics,
+    handleCancel
+  } = useProfileForm();
+  const uploadButton = (
+    <div>
+      <Icon type="plus" />
+      <div className="ant-upload-text">Upload</div>
+    </div>
+  );
+  return (
+    <div className="clearfix">
+      <Upload
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        listType="picture-card"
+        fileList={values.fileList}
+        onPreview={handlePreview}
+        onChange={handleChangePics}
+      >
+        {values.fileList.length >= 8 ? null : uploadButton}
+      </Upload>
+      <Modal
+        visible={values.previewVisible}
+        footer={null}
+        onCancel={handleCancel}
+      >
+        <img
+          alt="example"
+          style={{ width: "100%" }}
+          src={values.previewImage}
+        />
+      </Modal>
+    </div>
+  );
+};
 
 export default PicturesWall;
