0893fdfcd2c8fa029890fa1a549cf4d9ddf489d2
valentin Lecoq-Vallon <vlecoq-v@student.42.fr>
2019-09-04 16:40:32 +0200

    cleaned a bit

--- client/src/Scenes/Chat/ChatContext.js
+++ client/src/Scenes/Chat/ChatContext.js
@@ -7,35 +7,30 @@ const ChatContext = React.createContext([{}, () => {}]);
 const ChatProvider = props => {
   const [socketContext, authContext] = useContext(AuthContext);
   const [chatTarget, setChatTarget] = useState({});
-  const basicContent = {
-    content:
-      "Welcome to the chat, click on one of your match to get a conversation going"
-  };
-  const [messages, setMessages] = useState([basicContent]);
+
+  const [messages, setMessages] = useState([]);
 
   // 2 EFFECTS EVERYTIME WE SELECT A PROFILE
   // 1. GET THE MESSAGES FROM THE DB
   //   getter: fetchData
   // 2. COMMUNICATE WITH THE BACK SOCKET WHO WE ARE TALKING WITH
   useEffect(() => {
+    const basicContent = {
+      content:
+        "Welcome to the chat, click on one of your match to get a conversation going"
+    };
+
     if (chatTarget.matched) {
       const fetchMsg = async () => {
         const result = await axios(
           `http://localhost:9000/api/chatMessages?uuidSource=${authContext.data.uuid}&target=${chatTarget.uuid}&jwt=${authContext.data.jwt}`
         );
-        return result.data
-          ? result.data
-          : [{ content: "you do not have a conversation yet" }];
+        return result.data ? result.data : [basicContent];
       };
       chatTarget.uuid && fetchMsg().then(setMessages);
       socketContext.socket.emit("joinRoom", chatTarget);
     } else {
-      setMessages([
-        {
-          content:
-            "Welcome to the chat, click on one of your match to get a conversation going"
-        }
-      ]);
+      setMessages([basicContent]);
     }
   }, [chatTarget, socketContext.socket, authContext.data]);
 
