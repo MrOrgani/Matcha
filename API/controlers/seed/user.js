c79a3394d2658e27a472bf2eb3bc0ed08155c265
Valentin LECOQ-VALLON <vlecoq-v@e1r1p9.42.fr>
2019-09-09 16:47:25 +0200

    merging

--- API/controlers/seed/user.js
+++ API/controlers/seed/user.js
@@ -2,13 +2,6 @@ const modelUser = require("../../models/modelSeed/modelUser");
 const uuid = require("uuid/v4");
 
 exports.gUsers = async (req, res) => {
-  const baseScore = JSON.stringify({
-    like: 0,
-    visit: 0,
-    block: 0
-  });
-  req.body.uuid = await uuid();
-  req.body.baseScore = baseScore;
   modelUser.gUsers(req);
   res.status(200).end();
 };
