a70d5678a95c417d07437037abd207d36b48b556
Maxime ORGANI <morgani@student.42.fr>
2019-09-10 19:26:47 +0200

    filter pop dist sort ord ok, working on tags

--- client/src/Scenes/Search/Components/UserList.js
+++ client/src/Scenes/Search/Components/UserList.js
@@ -5,6 +5,7 @@ import { UsersContext } from "./UsersContext";
 import { UserCardProvider } from "../../../Components/UserCards/UserCardContext";
 // import { AuthContext } from "../../../AuthContext";
 import "./UserList.css";
+const distFrom = require("distance-from");
 
 // ICI quand on passe en async opur filterUsers on a un bug etrange lie au
 // fait qu'on attende la reponse de l'api dans UsersContext;
@@ -19,37 +20,81 @@ const UserList = () => {
   // const filteredUserList = filterUsers(
   // [filtersValue.gender, filtersValue.age, filtersValue.pop], usersValue.users;
   // );
-
+  // console.log("filtervales", filtersValue.tags);
   useEffect(() => {
     const filterUsers = (filters, users) => {
-      // console.log(filters);
-      // console.log(users);
       const genderFiltered =
         !filters[0] || filters[0] === "both"
           ? users
           : users.filter(user => user.gender === filters[0]);
-
-      // const ageFiltered = genderFiltered.filter(
-      //   user => user.age.low >= filters[1][0] && user.age.low <= filters[1][1]
-      // );
-      setFilteredUserList(
-        genderFiltered
-          .filter(
-            user =>
-              user.age.low >= filters[1][0] && user.age.low <= filters[1][1]
-          )
-          .filter(
-            user =>
-              user.score.low >= filters[2][0] && user.score.low <= filters[2][1]
-          )
-      );
+      const filtersfiltered = genderFiltered
+        .filter(user => user.age >= filters[1][0] && user.age <= filters[1][1])
+        .filter(
+          user =>
+            user.score.low >= filters[2][0] && user.score.low <= filters[2][1]
+        )
+        .filter(
+          user =>
+            distFrom(session.location).to(user.location).distance.v <=
+            filters[3]
+        )
+        .filter(user => user.hobbies.map(tag => !filters[6].includes(tag)));
+      // user.hobbies.filter(tag => !filters[6].includes(tag)));
+      console.log("filiter6", filters[6]);
+      if (filtersValue.sort) {
+        if (filters[4] === "age")
+          setFilteredUserList(
+            filtersfiltered.sort((a, b) =>
+              filters[5]
+                ? parseFloat(a.age) - parseFloat(b.age)
+                : parseFloat(b.age) - parseFloat(a.age)
+            )
+          );
+        else if (filters[4] === "pop")
+          setFilteredUserList(
+            filtersfiltered.sort((a, b) =>
+              filters[5]
+                ? parseFloat(a.score.low) - parseFloat(b.score.low)
+                : parseFloat(b.score.low) - parseFloat(a.score.low)
+            )
+          );
+        else if (filters[4] === "dist")
+          setFilteredUserList(
+            filtersfiltered.sort((a, b) =>
+              filters[5]
+                ? parseFloat(
+                    distFrom(session.location).to(a.location).distance.v
+                  ) -
+                  parseFloat(
+                    distFrom(session.location).to(b.location).distance.v
+                  )
+                : parseFloat(
+                    distFrom(session.location).to(b.location).distance.v
+                  ) -
+                  parseFloat(
+                    distFrom(session.location).to(a.location).distance.v
+                  )
+            )
+          );
+      } else setFilteredUserList(filtersfiltered);
     };
     filterUsers(
-      [filtersValue.gender, filtersValue.age, filtersValue.pop],
+      [
+        filtersValue.gender,
+        filtersValue.age,
+        filtersValue.pop,
+        filtersValue.dist,
+        filtersValue.sort,
+        filtersValue.ord,
+        filtersValue.tags
+      ],
       usersValue.users
     );
   }, [filtersValue, usersValue]);
 
+  // filteredUserList.map(user =>
+  //   console.log(distFrom(session.location).to(user.location).distance.v)
+  // );
   return (
     <div className="containerUL">
       {filteredUserList.map(user => {
