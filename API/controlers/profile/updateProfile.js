10cfd2229e4333004dae32116810b55a14df86d4
Maxime ORGANI <morgani@student.42.fr>
2019-09-11 11:51:42 +0200

    Get user location OK

--- API/controlers/profile/updateProfile.js
+++ API/controlers/profile/updateProfile.js
@@ -1,6 +1,8 @@
 const {
   modelUpdateProfile
 } = require("../../models/modelProfile/modelUpdateProfile");
+const modelFindOne = require("../../models/modelUser/modelFindOne");
+const cleanUserData = require("../user/cleanUserData");
 
 // UPDATE INFOS  FROM PROFILE PAGE
 exports.updateProfile = async (req, res) => {
@@ -9,5 +11,21 @@ exports.updateProfile = async (req, res) => {
     res.status(200).send(data);
   } catch (err) {
     res.status(401).send(err);
+  }
+};
+
+exports.updateLocation = async (req, res) => {
+  try {
+    let data = await modelFindOne(
+      req.query.uuidSource,
+      "uuid",
+      `SET u.location = [${req.body.lat}, ${req.body.lon}]`
+    );
+    // console.log("data ", data);
+    data = cleanUserData(data[0]._fields[0].properties);
+    // console.log("data after clean ", data);
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(400).send(err);
   }
 };
--- API/models/modelUser/modelFindOne.js
+++ API/models/modelUser/modelFindOne.js
@@ -3,16 +3,20 @@ const { initNeo4j } = require("../initNeo4j");
 const session = initNeo4j();
 
 module.exports = async function modelFindOne(value, category, option = "") {
-  const data = await session.run(
-    `WITH {category} AS propname
+  try {
+    const data = await session.run(
+      `WITH {category} AS propname
     MATCH(u:User)
     WHERE u[propname] = $value
     ${option}
     RETURN u`,
-    {
-      value: value,
-      category: category
-    }
-  );
-  return data.records;
+      {
+        value: value,
+        category: category
+      }
+    );
+    return data.records;
+  } catch (err) {
+    console.log(err);
+  }
 };
