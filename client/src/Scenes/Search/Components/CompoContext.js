177bb890f0ba7a7d7b8b11241bad5254b8828af4
Maxime ORGANI <morgani@student.42.fr>
2019-09-19 11:59:19 +0200

    bettered the search

---
+++ client/src/Scenes/Search/Components/CompoContext.js
@@ -0,0 +1,80 @@
+import React, { useState, useContext, useEffect } from "react";
+import UserList from "./UserList";
+// import { UsersProvider } from "./Components/UsersContext";
+import "../public/stylesheet/style.css";
+import Filters from "./Filters";
+import axios from "axios";
+import { AuthContext } from "../../../AuthContext";
+import UserMap from "./UserMap";
+import UserMatch from "./UserMatch";
+import { UsersContext } from "./UsersContext";
+
+export default function CompoContext() {
+  const [, authContext] = useContext(AuthContext);
+  const [state, setState] = useState({
+    lat: "",
+    lon: ""
+  });
+  const [map, setMap] = useState(false);
+  //   const [fire, setFire] = useState(false);
+  const { data } = authContext;
+  const [usersValue, filtersValue] = useContext(UsersContext);
+
+  if (!data.isComplete) window.location = "/Profile";
+
+  useEffect(
+    () =>
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          setState({
+            lat: position.coords.latitude,
+            lon: position.coords.longitude
+          });
+        },
+        async () => {
+          const api = await fetch("https://ipapi.co/json");
+          const api_json = await api.json();
+          setState({
+            lat: api_json.latitude,
+            lon: api_json.longitude
+          });
+        }
+      ),
+    []
+  );
+
+  useEffect(() => {
+    async function updateLocation() {
+      const userData = await axios
+        .put(
+          `http://localhost:9000/api/user/profile?uuidSource=${data.uuid}`,
+          state
+        )
+        .catch(err => console.log(err));
+      sessionStorage.setItem("data", JSON.stringify(userData.data));
+    }
+    if (state.lat !== "") updateLocation();
+  }, [state, data.uuid]);
+
+  function handleClick(value) {
+    if (value === "map") {
+      setMap(!map);
+      filtersValue.setFire(false);
+    } else if (value === "fire") {
+      usersValue.setMatchUsers([]);
+      setMap(false);
+      filtersValue.setFire(!filtersValue.fire);
+    }
+  }
+
+  return (
+    <>
+      <div style={{ display: "flex" }}>
+        <Filters onClick={handleClick} map={map} fire={filtersValue.fire} />
+      </div>
+      {map && <UserMap />}
+      {filtersValue.fire && <UserMatch />}
+      {!filtersValue.fire && !map && <UserList />}
+    </>
+  );
+}
--- client/src/Scenes/Search/Search.js
+++ client/src/Scenes/Search/Search.js
@@ -1,82 +1,12 @@
-import React, { useState, useContext, useEffect } from "react";
-import UserList from "./Components/UserList";
+import React from "react";
 import { UsersProvider } from "./Components/UsersContext";
-import "./public/stylesheet/style.css";
-import Filters from "./Components/Filters";
-import axios from "axios";
-import Notifications from "react-notify-toast";
-import { AuthContext } from "../../AuthContext";
-import UserMap from "./Components/UserMap";
-import UserMatch from "./Components/UserMatch";
+import CompoContext from "./Components/CompoContext";
 
 const Search = () => {
-  const [, authContext] = useContext(AuthContext);
-  const [state, setState] = useState({
-    lat: "",
-    lon: ""
-  });
-  const [map, setMap] = useState(false);
-  const [fire, setFire] = useState(false);
-  const { data } = authContext;
-
-  if (!data.isComplete) window.location = "/Profile";
-
-  useEffect(
-    () =>
-      navigator.geolocation.getCurrentPosition(
-        position => {
-          setState({
-            lat: position.coords.latitude,
-            lon: position.coords.longitude
-          });
-        },
-        async () => {
-          const api = await fetch("https://ipapi.co/json");
-          const api_json = await api.json();
-          setState({
-            lat: api_json.latitude,
-            lon: api_json.longitude
-          });
-        }
-      ),
-    []
-  );
-
-  useEffect(() => {
-    async function updateLocation() {
-      const userData = await axios
-        .put(
-          `http://localhost:9000/api/user/profile?uuidSource=${data.uuid}`,
-          state
-        )
-        .catch(err => console.log(err));
-      sessionStorage.setItem("data", JSON.stringify(userData.data));
-    }
-    if (state.lat !== "") updateLocation();
-  }, [state, data.uuid]);
-
-  function handleClick(value) {
-    if (value === "map") {
-      setMap(!map);
-      setFire(false);
-    } else if (value === "fire") {
-      setMap(false);
-      setFire(!fire);
-    }
-  }
-
-  // console.log("map", map);
-
   return (
     <div>
       <UsersProvider>
-        <Notifications />
-        <div style={{ display: "flex" }}>
-          <Filters onClick={handleClick} map={map} fire={fire} />
-        </div>
-        {map && <UserMap />}
-        {fire && <UserMatch />}
-        {!fire && !map && <UserList />}
+        <CompoContext />
       </UsersProvider>
     </div>
   );
