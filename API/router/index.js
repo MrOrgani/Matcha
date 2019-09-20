26853ace2259b740330058cfdffbf1854c9512b9
Valentin LECOQ-VALLON <vlecoq-v@e1r8p19.42.fr>
2019-09-20 11:58:12 +0200

    cleaned the Oauth & app

--- API/router/index.js
+++ API/router/index.js
@@ -6,7 +6,7 @@ const router = require("express").Router();
 // ROUTES
 router
   .use("/api/user", require("./routes/user"))
-  // .use("/api/Oauth", require("./routes/routeOauth"))
+  .use("/api/Oauth", require("./routes/routeOauth"))
   .use("/api/rel", require("./routes/routeRel"))
   .use("/api/notif", require("./routes/routeNotif"))
   .use("/api/chat", require("./routes/routeChat"))
