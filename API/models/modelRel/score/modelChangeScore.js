93dad750245c1b877877f712769f2fa716015e27
valentin Lecoq-Vallon <vlecoq-v@student.42.fr>
2019-09-23 14:32:16 +0200

    need to work on the caht

--- API/models/modelRel/score/modelChangeScore.js
+++ API/models/modelRel/score/modelChangeScore.js
@@ -8,7 +8,7 @@ exports.modelChangeScore = async (req, scoreChange) => {
   const cypher =
     scoreChange >= 0
       ? "MATCH (s:User {uuid:$target}) SET s.score = CASE WHEN s.score + $scoreChange > 100 THEN 100 ELSE s.score + $scoreChange END"
-      : "MATCH (s:User {uuid:$target}) SET s.score = CASE WHEN s.score + $scoreChange < 100 THEN 0 ELSE s.score + $scoreChange END";
+      : "MATCH (s:User {uuid:$target}) SET s.score = CASE WHEN s.score + $scoreChange < 0 THEN 0 ELSE s.score + $scoreChange END";
   try {
     await session.run(cypher, { target: req.target, scoreChange: scoreChange });
   } catch (err) {
