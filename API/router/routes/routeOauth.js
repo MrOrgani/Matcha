e006e6f28af50fd0e3fb70e6efa47cf67b985e57
Valentin LECOQ-VALLON <vlecoq-v@e2r7p22.42.fr>
2019-10-01 11:28:00 +0200

    Oauth error

--- API/router/routes/routeOauth.js
+++ API/router/routes/routeOauth.js
@@ -6,13 +6,11 @@ router.route("/42").get(passport.authenticate("42"));
 router
   .route("/42/redirect")
   .get(
-    passport.authenticate("42", { failureRedirect: "api/Oauth/42/failure" }),
+    passport.authenticate("42", { failureRedirect: "failure" }),
     (req, res) => passportSuccess(req, res)
   );
 router
   .route("/42/failure")
-  .get(passport.authenticate("42"), (req, res) =>
-    res.redirect("htt[://localhost:3000")
-  );
+  .get((req, res) => res.redirect("http://localhost:3000"));
 
 module.exports = router;
