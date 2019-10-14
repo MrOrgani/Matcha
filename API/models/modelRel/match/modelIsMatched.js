a1566a9edf467fb16ca6b04b59185bfbd8bcb965
valentin Lecoq-Vallon <vlecoq-v@student.42.fr>
2019-10-14 14:14:12 +0200

    securizing the cyphers some more

--- API/models/modelRel/match/modelIsMatched.js
+++ API/models/modelRel/match/modelIsMatched.js
@@ -4,9 +4,10 @@ const session = initNeo4j();
 async function modelIsMatched(req) {
   try {
     const data = await session.run(
-      `MATCH (u:User {uuid:"${req.uuidSource}"})
-        -[r:MATCHED]-(n:User {uuid:"${req.target}"})
-        RETURN r`
+      `MATCH (u:User {uuid:"uuidSource"})
+        -[r:MATCHED]-(n:User {uuid:"target"})
+        RETURN r`,
+      req
     );
     // REPLACED BY A BOOLEAN ANSWER
     return data.records.length > 0 ? true : false;
