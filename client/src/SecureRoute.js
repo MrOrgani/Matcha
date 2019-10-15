69b50d638e9028fe5becc8d9610661184c10bd0d
valentin Lecoq-Vallon <vlecoq-v@student.42.fr>
2019-10-15 16:18:52 +0200

    adjustments

--- client/src/SecureRoute.js
+++ client/src/SecureRoute.js
@@ -29,7 +29,7 @@ export default ({ component: Component, conditions: Conditions, ...rest }) => {
           window.location = "/Profile";
           return <Redirect to="/Profile" />;
         }
-        // else return <Redirect to="/" />;
+        else return <Redirect to="/" />;
       }}
     />
   );
